4f4906112fc6ca0428c7d4b7d2d4455660e89363
Travis Park <travispk268@gmail.com>
2023-03-18 11:51:52 -0700

    Add project files.

---
+++ CalculateVectorSums/kernel.cu
@@ -0,0 +1,14 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+
+int main()
+{
+	int a[] = { 1,2,3 };
+	int b[] = { 4,5,6 };
+	int c[sizeof(a) / sizeof(int)] = {0};
+}
+

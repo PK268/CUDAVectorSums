3f652bb5890ac67ddc58f253d99125b7bc3d220b
Travis Park <travispk268@gmail.com>
2023-03-18 14:44:32 -0700

    Works with gpu

--- CalculateVectorSums/kernel.cu
+++ CalculateVectorSums/kernel.cu
@@ -4,11 +4,35 @@
 
 #include <stdio.h>
 
+__global__ void vectorAdd(int* a, int* b, int* c)
+{
+	int i = threadIdx.x;
+	c[i] = a[i] + b[i];
+
+	return;
+}
 
 int main()
 {
-	int a[] = { 1,2,3 };
-	int b[] = { 4,5,6 };
-	int c[sizeof(a) / sizeof(int)] = {0};
+	int a[] = { 1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3 };
+	int b[] = { 4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6,4,5,6 };
+	int c[sizeof(a) / sizeof(int)] = { 0 };
+
+	int* cudaA = 0;
+	int* cudaB = 0;
+	int* cudaC = 0;
+
+	cudaMalloc(&cudaA, sizeof(a));
+	cudaMalloc(&cudaB, sizeof(b));
+	cudaMalloc(&cudaC, sizeof(c));
+
+	cudaMemcpy(cudaA, a, sizeof(a), cudaMemcpyHostToDevice);
+	cudaMemcpy(cudaB, b, sizeof(b), cudaMemcpyHostToDevice);
+
+	vectorAdd << <2, sizeof(a) / sizeof(int) >> > (cudaA, cudaB, cudaC);
+
+	cudaMemcpy(c, cudaC, sizeof(c), cudaMemcpyDeviceToHost);
+
+	return;
 }
 
